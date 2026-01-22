46c0ae78605bd0201c2ae8f17e3425b49b8251d3
Varshaps07 <varshasonnad28@gmail.com>
2026-01-22 22:09:10 +0530

    verify commit

--- middleware/verifyAdmin.js
+++ middleware/verifyAdmin.js
@@ -1,22 +1,22 @@
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 
-const verifyAdmin = (req, res, next) => {
-  try {
-    const token = req.cookies.token;
+// const verifyAdmin = (req, res, next) => {
+//   try {
+//     const token = req.cookies.token;
 
-    if (!token)
-      return res.status(401).json({ msg: "Not logged in" });
+//     if (!token)
+//       return res.status(401).json({ msg: "Not logged in" });
 
-    const decoded = jwt.verify(token, "SECRET_KEY");
+//     const decoded = jwt.verify(token, "SECRET_KEY");
 
-    if (decoded.role !== "admin")
-      return res.status(403).json({ msg: "Access denied" });
+//     if (decoded.role !== "admin")
+//       return res.status(403).json({ msg: "Access denied" });
 
-    next();
-  } catch (err) {
-    return res.status(401).json({ msg: "Session expired" });
-  }
-};
+//     next();
+//   } catch (err) {
+//     return res.status(401).json({ msg: "Session expired" });
+//   }
+// };
 
-module.exports = verifyAdmin;
+// module.exports = verifyAdmin;
 
